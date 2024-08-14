66f481478a23fa7513ebdf252c5038e014b7bb22
novendra <novendraar@gmail.com>
2024-08-14 13:06:45 +0700

    Update tampilan

--- app.py
+++ app.py
@@ -87,54 +87,68 @@ def load_data():
 def main():
     st.markdown("""
                 <style>
-                    .bold-text { font-weight: bold; }
+                    .bold-text { font-weight: bold; color: #000; font-size: 30px;}
                     .custome-header { font-size: 39px; font-family: 'Arial'; color: #4682B4; }
                     .caption-kecil { font-size: 35px; font-family: 'Arial';} 
                     .header-daftar { font-size: 20px; font-family: 'Arial';}
-                    .list-daftar {font-size: 17px; font-family: 'Arial'; margin= 0; padding= 0;}
+                    .list-daftar {font-size: 17px; font-family: 'Arial'; margin: 0; padding: 0;}
                     .centered { text-align: center; }
                     .header-sub { font-size: 35px; font-family: 'Arial'; color: #cf9823;}
-                    .list-daftar {font-size: 17px; font-family: 'Arial'; margin= 0; padding= 0;}
+                    .list-daftar {font-size: 17px; font-family: 'Arial'; margin: 0; padding: 0;}
+                    .boxes-color {background-color: #e9e9e9 ; border-radius: 15px; padding: 10px 20px 10px 20px; margin: 10px;}
+                    .focus-area {color: #ff9500; font-weight: bold;}
                 </style>
-                <h1 class="custome-header bold-text no-space">Project Association with Streamlit</h1>
-                <h2 class="caption-kecil bold-text no-space">Big Data FGA ITI 2024</h2>
+                <h1 class="custome-header">Project Association with Streamlit</h1>
+                <h2 class="caption-kecil">Big Data FGA ITI 2024</h2>
                 <hr style="border: 1px solid #000; width: 100%;"/>
                 """, unsafe_allow_html=True)
     st.markdown("""
-                <p class ="header-daftar bold-text">Daftar Anggota Tim:</p>
-                <p class ="list-daftar bold-text">1. Marsheila Prima Andika <br>2. Megawati Aswiya Putri <br>3. Jilaan Hani Safitri <br>4. Novendra Aliffian Ramadhan</p>
-    """, unsafe_allow_html=True)
+                <div class="boxes-color">
+                    <p class ="header-daftar bold-text">Daftar Anggota Tim:</p>
+                    <p class ="list-daftar bold-text">1. Marsheila Prima Andika <br>2. Megawati Aswiya Putri <br>3. Jilaan Hani Safitri <br>4. Novendra Aliffian Ramadhan</p>
+                </div>
+                """, unsafe_allow_html=True)
     st.markdown("""
                 <div class="centered">
                     <h1>Bakery Data Analysis</h1>
                 </div>
-    """, unsafe_allow_html=True)
+                """, unsafe_allow_html=True)
     st.image("gmbr/bread.jpg")
     st.markdown("""
                 <h1 class="header-sub">Project Understanding</h1>
-                <h2 class="bold-text">1. Business Understanding</h2>
-                <p>Analisis Market Basket:<br>
-                - Meneliti kumpulan item untuk menemukan hubungan antar item dalam konteks bisnis.<br>
-                - Menggunakan teknik Association Rule Mining.<br>
-                Algoritma Apriori:<br>
-                - Digunakan dalam Association Rule Mining untuk menemukan pola frekuensi itemset yang sering muncul bersama dalam transaksi.<br>
-                Model analisis yang digunakan dalam Market Basket Analysis adalah Association Rule Mining dengan algoritma Apriori.<br>
+                <div class="boxes-color">
+                    <h2 class="bold-text">1. Business Understanding</h2>
+                    <p>Analisis Market Basket:<br>
+                    - Meneliti kumpulan item untuk menemukan hubungan antar item dalam konteks bisnis.<br>
+                    - Menggunakan teknik Association Rule Mining.<br>
+                    Algoritma Apriori:<br>
+                    - Digunakan dalam Association Rule Mining untuk menemukan pola frekuensi itemset yang sering muncul bersama dalam transaksi.<br>
+                    Model analisis yang digunakan dalam Market Basket Analysis adalah Association Rule Mining dengan algoritma Apriori.<br></p>
+                </div>
+                <div class="boxes-color">
                 <h2 class="bold-text">2. Data Understanding</h2>
-                <p>Dataset ini milik "The Bread Basket", sebuah toko roti yang berlokasi di Edinburgh. Dataset ini menyediakan detail transaksi pelanggan yang memesan berbagai item dari toko roti ini secara daring selama periode waktu dari tahun 2016 dan 2017 dari toko roti daring. Dataset ini memiliki 20507 entri, lebih dari 9000 transaksi, dan 4 kolom.<br></p>
-                <p>Data diambil dari kaggle sebagai berikut : https://www.kaggle.com/code/akashdeepkuila/market-basket-analysis</p>
-                <h2 class="bold-text">3. Teori Asosiasi</h2>
-                <h4>1. Support</h4>
-                <p>Mengukur frekuensi kemunculan item atau itemset. <br>Rumus => Support(A) = (Jumlah transaksi yang mengandung A) / (Total transaksi</p>
-                <h4>2. Confidence</h4>
-                <p>Mengukur seberapa sering item B muncul dalam transaksi yang mengandung item A. <br>Rumus => Confidence(A→B) = Support(A ∪ B) / Support(A).</p>
-                <h4>3. Lift</h4>
-                <p>Mengukur kekuatan aturan dengan memperhitungkan frekuensi kemunculan item B. <br>Rumus => Lift(A→B) = Confidence(A→B) / Support(B).</p>
+                    <p>Dataset ini milik "The Bread Basket", sebuah toko roti yang berlokasi di Edinburgh. Dataset ini menyediakan detail transaksi pelanggan yang memesan berbagai item dari toko roti ini secara daring selama periode waktu dari tahun 2016 dan 2017 dari toko roti daring. Dataset ini memiliki 20507 entri, lebih dari 9000 transaksi, dan 4 kolom.<br></p>
+                    <p>Data diambil dari kaggle sebagai berikut : https://www.kaggle.com/code/akashdeepkuila/market-basket-analysis</p>
+                </div>
+                <div class="boxes-color">       
+                    <h2 class="bold-text">3. Teori Asosiasi</h2>
+                    <h4 style="font-size: 22px;">1. Support</h4>
+                    <p>Mengukur frekuensi kemunculan item atau itemset. <br><strong>Rumus => Support(A) = (Jumlah transaksi yang mengandung A) / (Total transaksi)</strong></p>
+                    <h4 style="font-size: 22px;">2. Confidence</h4>
+                    <p>Mengukur seberapa sering item B muncul dalam transaksi yang mengandung item A. <br><strong>Rumus => Confidence(A→B) = Support(A ∪ B) / Support(A)</strong></p>
+                    <h4 style="font-size: 22px;">3. Lift</h4>
+                    <p>Mengukur kekuatan aturan dengan memperhitungkan frekuensi kemunculan item B. <br><strong>Rumus => Lift(A→B) = Confidence(A→B) / Support(B)</strong></p>
+                </div>
                 """, unsafe_allow_html=True)
     # Load the data
     bakeryDF = load_data()
     st.markdown("""
-                <h2 class="bold-text">Data Preparation</h2>
-                <h4>Tabel Dataframe Bakery</h4>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Data Preparation</h2>
+                </div>
+                <div class="boxes-color">
+                    <h4>Tabel Dataframe Bakery</h4>
+                </div>
                 """, unsafe_allow_html=True)
     st.dataframe(bakeryDF)
     st.write("Database dimension :", bakeryDF.shape)
@@ -147,22 +161,30 @@ def main():
 
     st.markdown("""
                 <h1 class="header-sub">Data Exploration and Visualization</h1>
-                <h2 class="bold-text">Visualization by Items</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Visualization by Items</h2>
+                </div>
                 """, unsafe_allow_html=True)
     itemFrequency = bakeryDF['Items'].value_counts().sort_values(ascending=False)
     plot_most_frequent_items(itemFrequency)
     st.markdown("""
-                <h2 class="bold-text">Visualization by Hours</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Visualization by Hours</h2>
+                </div>
                 """, unsafe_allow_html=True)
     peakHours = bakeryDF.groupby('Daypart')['Items'].count().sort_values(ascending=False)
     plot_peak_selling_hours(peakHours)
     st.markdown("""
-                <h2 class="bold-text">Visualization by Days</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Visualization by Days</h2>
+                </div>
                 """, unsafe_allow_html=True)
     mpd = bakeryDF.groupby('Day')['Items'].count().sort_values(ascending=False)
     plot_productive_day(mpd)
     st.markdown("""
-                <h2 class="bold-text">Visualization by Months</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Visualization by Months</h2>
+                </div>
                 """, unsafe_allow_html=True)
     mpm = bakeryDF.groupby('Month')['Items'].count().sort_values(ascending=False)
     plot_productive_month(mpm)
@@ -178,20 +200,26 @@ def main():
     data = pd.DataFrame(encodedData, columns=te.columns_)
     st.markdown("""
                 <h1 class="header-sub">Association Rules Generation</h1>
-                <h2 class="bold-text">Tabel Frekuensi Item</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Tabel Frekuensi Item</h2>
+                </div>
                 """, unsafe_allow_html=True)
     frequentItems = apriori(data, use_colnames=True, min_support=0.02)
     frequentItems['itemsets'] = frequentItems['itemsets'].apply(lambda x: list(x))
     st.dataframe(frequentItems)
     st.markdown("""
-                <h2 class="bold-text">Tabel Rules Asosiasi</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Tabel Rules Asosiasi</h2>
+                </div>
                 """, unsafe_allow_html=True)   
     rules = association_rules(frequentItems, metric="lift", min_threshold=1)
     rules.antecedents = rules.antecedents.apply(lambda x: next(iter(x)))
     rules.consequents = rules.consequents.apply(lambda x: next(iter(x)))
     st.dataframe(rules)
     st.markdown("""
-                <h2 class="bold-text">Rules Visualization</h2>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Rules Visualization</h2>
+                </div>
                 """, unsafe_allow_html=True) 
     network_A = list(rules["antecedents"].unique())
     network_B = list(rules["consequents"].unique())
@@ -208,8 +236,10 @@ def main():
 
     plot_network_graph(G)
     st.markdown("""
-                <h2 class="bold-text">Refining Rules</h2>
-                <p>Confidence untuk consequent yang paling sering muncul selalu memiliki nilai yang tinggi, walaupun asosiasinya sangat rendah, sehingga memberikan jawaban yang rancu. <br>Karena kopi merupakan produk best seller dan dapat direkomendasikan bersama produk lainnya, maka kita akan drop rules tersebut untuk menemukan rules lain yang tidak diketahui.</p>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Refining Rules</h2>
+                    <p>Confidence untuk consequent yang paling sering muncul selalu memiliki nilai yang tinggi, walaupun asosiasinya sangat rendah, sehingga memberikan jawaban yang rancu. <br>Karena kopi merupakan produk best seller dan dapat direkomendasikan bersama produk lainnya, maka kita akan drop rules tersebut untuk menemukan rules lain yang tidak diketahui.</p>
+                </div>
                 """, unsafe_allow_html=True)  
     index_names = rules[rules['consequents'] == 'Coffee'].index
     refinedRules = rules.drop(index_names).sort_values('lift', ascending=False)
@@ -219,11 +249,15 @@ def main():
 
     st.markdown("""
                 <h1 class="header-sub">Summary</h1>
-                <h2 class="bold-text">Insights</h2>
-                <p class="bold-text">- Kopi merupakan produk best seller di toko roti ini dan kopi menunjukkan asosiasi dengan 8 produk lainnya.<br>- Lebih dari 11% pecinta kopi juga membeli kue, sedangkan 10% lainnya membeli pastry.<br>- Lebih dari 16% pecinta teh akan membeli kue, sedangkan lebih dari 20% pecinta kue akan membeli teh.<br>- Lebih dari 33% pecinta pastry akan membeli roti, sedangkan hanya hampir 9% pembeli pastry akan membeli roti</p>
-                <h2 class="bold-text">Business Strategy</h2>
-                <p>Terdapat beberapa strategi yang dapat dilakukan oleh toko roti untuk meningkatkan penjualan berdasarkan hasil analisis asosiasi yang telah dilakukan antara kopi dan 8 produk lainnya, yaitu:<br></p>
-                <p class="bold-text">- Promosi diskon pada produk-produk tersebut dapat menarik pelanggan untuk membeli kopi atau sebaliknya.<br>- Menata produk-produk tersebut di dekat konter pemesanan kopi untuk menggoda pelanggan agar membelinya</p>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Insights</h2>
+                    <p class="focus-area">- Kopi merupakan produk best seller di toko roti ini dan kopi menunjukkan asosiasi dengan 8 produk lainnya.<br>- Lebih dari 11% pecinta kopi juga membeli kue, sedangkan 10% lainnya membeli pastry.<br>- Lebih dari 16% pecinta teh akan membeli kue, sedangkan lebih dari 20% pecinta kue akan membeli teh.<br>- Lebih dari 33% pecinta pastry akan membeli roti, sedangkan hanya sekitar 9% pembeli roti akan membeli pastry</p>
+                </div>
+                <div class="boxes-color">
+                    <h2 class="bold-text">Business Strategy</h2>
+                    <p>Terdapat beberapa strategi yang dapat dilakukan oleh toko roti untuk meningkatkan penjualan berdasarkan hasil analisis asosiasi yang telah dilakukan antara kopi dan 8 produk lainnya, yaitu:<br></p>
+                    <p class="focus-area">- Promosi diskon pada produk-produk tersebut dapat menarik pelanggan untuk membeli kopi atau sebaliknya.<br>- Menata produk-produk tersebut di dekat konter pemesanan kopi untuk menggoda pelanggan agar membelinya</p>
+                </div>
                 """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
